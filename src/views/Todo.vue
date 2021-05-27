adf7e0a46a4cee1f356b44a38d4a497350e807c2
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-05-27 14:17:57 +0200

    Chap - 20 - Delete task

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="home">
-    <v-list class="pt-0" flat>
+    <v-list class="pt-0
+    done: false," flat>
       <div v-for="task in tasks" :key="task.id">
-        <v-list-item>
-          <template v-slot:default="{ active }">
+        <v-list-item
+        @click="doneTask(task.id)"
+        :class="{'blue lighten-4': task.done}">
+          <template v-slot:default>
             <v-list-item-action>
-              <v-checkbox :input-value="active" color="primary"></v-checkbox>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
+              <v-list-item-title
+              :class="{'text-decoration-line-through': task.done}">{{ task.title }}</v-list-item-title>
             </v-list-item-content>
           </template>
         </v-list-item>
@@ -28,17 +32,26 @@ export default {
         {
           id: 1,
           title: "Wake up",
+          done: false,
         },
         {
           id: 2,
           title: "Get bananas",
+          done: false,
         },
         {
           id: 3,
           title: "Eat bananas",
+          done: false,
         },
       ],
     };
   },
+  methods: {
+    doneTask(id) {
+      let task = this.tasks.filter(task => task.id === id)[0]
+      task.done = !task.done 
+    }
+  }
 };
 </script>
