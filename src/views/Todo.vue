897819447f6571e6c9d1a265083b4f6cf481717d
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-05-27 14:27:02 +0200

    Chap - 21 - Add task

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="home">
-    <v-list class="pt-0
-    done: false," flat>
+    <v-list
+      class="pt-0
+    done: false,"
+      flat
+    >
       <div v-for="task in tasks" :key="task.id">
         <v-list-item
-        @click="doneTask(task.id)"
-        :class="{'blue lighten-4': task.done}">
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-4': task.done }"
+        >
           <template v-slot:default>
             <v-list-item-action>
               <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
@@ -13,8 +17,18 @@
 
             <v-list-item-content>
               <v-list-item-title
-              :class="{'text-decoration-line-through': task.done}">{{ task.title }}</v-list-item-title>
+                :class="{ 'text-decoration-line-through': task.done }"
+                >{{ task.title }}</v-list-item-title
+              >
             </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn 
+              @click.stop="deleteTask(task.id)"
+              icon>
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
           </template>
         </v-list-item>
         <v-divider></v-divider>
@@ -49,9 +63,13 @@ export default {
   },
   methods: {
     doneTask(id) {
-      let task = this.tasks.filter(task => task.id === id)[0]
-      task.done = !task.done 
+      let task = this.tasks.filter(task => task.id === id)[0];
+      task.done = !task.done;
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id)
+
     }
-  }
+  },
 };
 </script>
