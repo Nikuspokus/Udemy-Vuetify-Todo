1bb268e1615354a3d8270b0f7056117b3d2c58b6
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-06-02 09:57:20 +0200

    Chap - 49 - Module - 9 - FINI

--- src/components/Todo/TaskMenu.vue
+++ src/components/Todo/TaskMenu.vue
@@ -7,7 +7,13 @@
     </template>
 
     <v-list>
-      <v-list-item v-for="(item, i) in items" :key="i">
+      <v-list-item 
+      v-for="(item, index) in items" 
+      :key="index"
+      @click="handleClick(index)">
+        <v-list-item-icon>
+          <v-icon v-text="item.icon"></v-icon>
+        </v-list-item-icon>
         <v-list-item-title>{{ item.title }}</v-list-item-title>
       </v-list-item>
     </v-list>
@@ -16,17 +22,37 @@
 
 <script>
 export default {
-    data: () => ({
-      items: [
-        { title: 'Click Me' },
-        { title: 'Click Me' },
-        { title: 'Click Me' },
-        { title: 'Click Me 2' },
-      ],
-    }),
+  data: () => ({
+    items: [
+      {
+        title: "Edit",
+        icon: "mdi-pencil",
+        click() {
+          console.log("edit");
+        },
+      },
+      {
+        title: "Due date",
+        icon: "mdi-calendar",
+        click() {
+          console.log("date");
+        },
+      },
+      {
+        title: "Delete",
+        icon: "mdi-delete",
+        click() {
+          console.log("delete");
+        },
+      },
+    ],
+  }),
+  methods: {
+    handleClick(index) {
+      this.items[index].click()
+    }
   }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
