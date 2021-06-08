09d07b03ba07c91090c8af146a158c757a64c2c5
Marquillier <nicolas.marquillier@gmail.com>
2021-06-08 15:47:02 +0200

    Chap 76 - module 12 - FINI

--- src/App.vue
+++ src/App.vue
@@ -35,34 +35,28 @@
         ></v-img>
       </template>
 
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-app-bar-title>Vuetify Todo</v-app-bar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
+      <v-container>
+        <v-row>
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-spacer></v-spacer>
+          <search />
+        </v-row>
+        <v-row>
+          <v-app-bar-title class="ml-4">Vuetify Todo</v-app-bar-title>
+        </v-row>
+      </v-container>
     </v-app-bar>
 
     <v-main>
       <router-view></router-view>
-      <snackbar/>
+      <snackbar />
     </v-main>
   </v-app>
 </template>
 
 <script>
 import Snackbar from "./components/Shared/Snackbar";
+import Search from "./components/Tools/Search.vue";
 
 export default {
   data: () => ({
@@ -73,7 +67,8 @@ export default {
     ],
   }),
   components: {
-    snackbar: Snackbar
+    snackbar: Snackbar,
+    search: Search,
   },
 };
 </script>
